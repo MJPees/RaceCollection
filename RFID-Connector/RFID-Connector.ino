00c67137d70bd4c4695576f4c28cc37e2658931b
MJPees <marc.pees@l-n-s.de>
2026-01-02 15:31:27 +0100

    changed payload of mac-address

--- RFID-Connector/RFID-Connector.ino
+++ RFID-Connector/RFID-Connector.ino
@@ -273,7 +273,7 @@ void getMacResponse(bool fromBle = false) {
   uint8_t mac[6];
   esp_read_mac(mac, ESP_MAC_BT);
   char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+  snprintf(macStr, sizeof(macStr), "%02X%02X%02X%02X%02X%02X",
             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   String response = "MSG_GET_MAC:" + String(macStr);
   Serial.println(response);
